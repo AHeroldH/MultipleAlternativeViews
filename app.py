e3820e32200f1a6395d0b7d02d962c6382c3ff64
Alberte Herold Hansen <28775759+AHeroldH@users.noreply.github.com>
2020-04-30 19:31:50 +0200

    Accommodated to the screen size again

--- app.py
+++ app.py
@@ -465,5 +465,5 @@ def return_current_scagnostics():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
-    #app.run()
+    #app.run(host='0.0.0.0', port='5000')
+    app.run()
